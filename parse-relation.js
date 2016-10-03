3d9ce37917ebb8689e474cbc5f13fed59e60c9f8
Jiaying (Karen) Peng <pengk@uber.com>
2016-10-02 17:49:13 -0700

    add relation parser

---
+++ parse-relation.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+var through = require('through2')
+var parseOSM = require('osm-pbf-parser')
+
+var osm = parseOSM()
+var first = true
+
+//var test = []
+const parser = through.obj(function (items, enc, next) {
+  items.forEach(function (item) {
+    if (item.type === 'relation' && (item.tags.type === 'multipolygon' || item.tags.type === 'building')) {
+      console.log(`${
+        first ? '' : ','
+      }"${item.id}":[${
+        item.members.map(function(item) {
+          return item.id
+      })}]`)
+      first = false
+      // if(test.indexOf(item.tags.type) === -1)
+      // console.log(item.tags.type)
+      // test.push(item.tags.type)
+    }
+  })
+  next()
+})
+parser.on('end', function () {
+  console.log('}')
+})
+
+console.log('{')
+fs.createReadStream('./sf.pbf')
+  .pipe(osm)
+  .pipe(parser)
